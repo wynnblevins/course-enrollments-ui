4d0d1831a4dbc7c366ea05f444282074a6f83a18
Wynn Blevins <wynnblevins@gmail.com>
2024-11-07 16:46:20 -0500

    Added home page with some description text

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,12 @@ import Grid from '@mui/material/Grid2';
 import CourseTeachersList from './components/CourseTeachersList/CourseTeachersList';
 import CoursesList from './components/CoursesList/CoursesList';
 import EnrollmentsList from './components/EnrollmentsList/EnrollmentsList';
-// require('dotenv').config({path:__dirname+'/../.env'}) 
+
 
 const App = () => {
   
   return (
-    <div className="App">
+    <div>
       <Navbar></Navbar>
       <Grid
         container
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div style={{ margin: '30px' }}>
+      <h1 style={{ textAlign: 'center' }}>About The Course Enrollments Project</h1>
+      <p>This application is a pretend enrollment system for a college.This application is a combination of React 
+        frontend project (https://github.com/wynnblevins/course-enrollments-ui) and a Java/Spring rest API 
+        (https://github.com/wynnblevins/CourseEnrollment).</p>
+      <p>This application is a pretend enrollment systemfor a college. You can create students, teachers, and courses, then go 
+        over to the enrollments and class instructors pages to enroll students in courses and assign teachers to courses.</p>
+      <p>The backend for this project is hosted on render (https://render.com) and it talks to a database thats hosted within
+        Aiven (https://aiven.io/).  All of this hosting is done on the "free" or "hobby" tiers.  What this means is that the servers
+        for this project are stopped when not in use.  With that in mind, your initial interactions with the application may seem 
+        delayed or laggy.  Please be patient.  The backend simply needs to wake up, then the application will function normally.
+      </p>
     </div>
   )
 };
